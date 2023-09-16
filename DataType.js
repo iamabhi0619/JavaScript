aa658cf161f9a100dfc7a8178eecbecd80ba3e29
Abhishek Kumar <iamabhi0619@gmail.com>
2023-09-16 12:35:33 +0530

    DataType understanding

---
+++ DataType.js
@@ -0,0 +1,39 @@
+const StudentId = 12303842
+let username = "iambhi0619" //let is a advance version of var
+var password = "12344321"
+address = "jharkhand" //js automaticaly use a variable
+let ACstate
+
+/*
+Prefer not to use var
+Because of issue in block scope and functional scope
+*/
+
+
+//changing the value
+// StudentId = "1237654" //cant cahange the value of constant
+username = "iambhi.com"
+password = "12123443"
+address = "Deoghar"
+
+console.log(StudentId);
+console.table([username, password, address, ACstate]);
+
+
+ // if we want to know the datatype of a variable
+ //use "typeof"
+ console.log("DataType of address:-",typeof address);
+ console.log("DataType of StudentId:-",typeof StudentId);
+/*
+SOME DATA TYPE:-
+    number => 2 to power 53
+    bigint
+    string => ""
+    boolean => true/false 
+    null => standalone value
+    undefined => 
+    symbol => unique
+
+
+    object
+*/
--- test.js
+++ test.js
@@ -1,1 +1,2 @@
 console.log("Kashish")
+console.log("Abhishek")
